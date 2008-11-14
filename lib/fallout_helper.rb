be4e00e49fc8dae376582dc9b6f45738c9fcca8c
Brian Clubb <bclubb@gmail.com>
2008-11-14 00:08:56 -0600

    trying to make autotest work

---
+++ lib/fallout_helper.rb
@@ -0,0 +1,39 @@
+Guess = Struct.new(:word, :matches)
+
+class FalloutHelper
+  attr_accessor :words
+  
+  
+  def initialize(words = Array.new)
+    self.words = words
+    map_words
+  end
+  
+  def map_words
+    self.words.each do |word|
+      #what
+    end
+  end
+  
+  def add_word word, matches
+    guess = Guess.new(word, matches)
+    self.words << guess
+  end
+  
+  def guessed_word(guessed_word, matches)
+    self.words.each do |word| 
+      if(word.word == guessed_word) 
+        word.matches = matches 
+      end 
+    end
+  end
+  
+  def possible_words
+    poss_words = Array.new
+    self.words.each do |word|
+      if(word.matches.nil?) 
+        poss_words << (word.word) 
+      end
+    end
+  end
+end
